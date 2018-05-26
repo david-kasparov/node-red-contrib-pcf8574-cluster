a799f8a35ab4ecd1e6885331019b9ae12a6d8eb8
David Kasparov <david@Davids-MacBook-Pro.local>
2018-05-26 21:34:14 +0400

    initial

---
+++ pcf8574-cluster.js
@@ -0,0 +1,13 @@
+module.exports = function(RED) {
+
+  function LowerCaseNode(config) {
+    RED.nodes.createNode(this,config);
+    var node = this;
+    node.on('input', function(msg) {
+      msg.payload = msg.payload.toLowerCase();
+      node.send(msg);
+    });
+  }
+
+  RED.nodes.registerType("pcf8574-cluster",LowerCaseNode);
+}
