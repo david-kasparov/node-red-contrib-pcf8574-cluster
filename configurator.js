d6811b5f850c099be4a6fa2cbba551115e48b326
David Kasparov <david@Davids-MacBook-Pro.local>
2018-05-27 20:59:21 +0400

    _

--- configurator.js
+++ configurator.js
@@ -3,23 +3,9 @@ module.exports = (RED) => {
   function ClusterConfiguratorNode(config) {
     RED.nodes.createNode(this, config);
 
-
-
-    this.warn(config.addresses);
-
-    /*this.addresses = JSON.parse(config.addresses);
+    this.addresses = JSON.parse(config.addresses);
     this.initialStates = JSON.parse(config.initialStates);
-
-    if (config.interrupts) {
-      this.interrupts = JSON.parse(config.interrupts);
-    }
-
-    clusterConfig = {
-        addresses: [0x20, 0x26],
-        initial_states: [true, true]
-      };
-
-    this.warn(config.addresses);*/
+    this.interrupts = JSON.parse(config.interrupts);
   }
 
   RED.nodes.registerType("cluster-configurator", ClusterConfiguratorNode);
