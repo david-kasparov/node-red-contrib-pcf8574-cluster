1c69ee7e03e972e0e44e2bd0d329633496523b4f
David Kasparov <david@Davids-MacBook-Pro.local>
2018-05-27 21:33:45 +0400

    _

---
+++ cluster-in.js
@@ -0,0 +1,29 @@
+const Cluster = require("./cluster");
+
+const instances = {};
+
+module.exports = (RED) => {
+
+  function ClusterInNode(n) {
+      RED.nodes.createNode(this, n);
+
+      let node = this;
+      let clusterConfig = RED.nodes.getNode(n.cluster);
+
+      if (!instances[n.cluster]) {
+        instances[n.cluster] = Cluster(clusterConfig);
+      }
+
+      const cluster = instances[n.cluster];
+
+      cluster.inputPin(n.pin, n.inverted)
+      .then(() => {
+        this.on('close', () => {
+          cluster.removeAllListeners();
+          cluster.disableAllInterrupts();
+        });
+      });
+  }
+
+  RED.nodes.registerType("cluster-in", ClusterInNode);
+};
